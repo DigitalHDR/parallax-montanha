3bee5b1f8379a1c3ae8618eab42c6b0f03708af6
Lucas Fernando <73972922+DigitalHDR@users.noreply.github.com>
2021-08-01 01:02:21 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+gsap.to("#bg", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    scale : 1.5
+})
+
+gsap.to("#man", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    scale : 0.5
+})
+
+gsap.to("#mountain_left", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : -630
+})
+
+gsap.to("#mountain_right", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 630
+})
+
+gsap.to("#clouds_1", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : 200
+})
+
+gsap.to("#clouds_2", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    x : -200
+})
+
+gsap.to("#text", {
+    scrollTrigger : {
+        scrub : 1
+    },
+    y : 600
+})
